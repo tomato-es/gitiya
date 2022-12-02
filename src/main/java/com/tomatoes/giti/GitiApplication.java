0d8f6184327ea5e16940541f231854330ee12427
gimhanul <gimhanuly@gmail.com>
2022-12-03 00:24:42 +0900

    SET :: project setting
    - project setting
    - application.yml 추가 =
    - docker-compose.yml database setting

---
+++ src/main/java/com/tomatoes/giti/GitiApplication.java
@@ -0,0 +1,13 @@
+package com.tomatoes.giti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomatoes/giti/GitiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomatoes.giti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
